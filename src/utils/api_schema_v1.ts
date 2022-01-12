347a9a15bb80285c459f77aa88feeb0bd3d9ecb2
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-12 09:57:08 -0500

    adds endpoint

--- src/utils/api_schema_v1.ts
+++ src/utils/api_schema_v1.ts
@@ -1,6 +1,9 @@
 const API_SCHEMA_V1 = {
   companies: {
     getCompanies: "/companies",
+    getCompanyContracts: function (companyId: number): string {
+      return `/companies/${companyId}/contracts`;
+    },
   },
   engineers: {
     getEngineers: "/engineers",
