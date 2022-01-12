2c89c0491911f40e9e06a496eb7d862a5ccc18b2
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-12 18:28:12 -0500

    creates dateformatter to utils

---
+++ src/utils/dateFormatter.ts
@@ -0,0 +1,22 @@
+import moment from "moment";
+
+const dateFormatter = {
+  formatDateToHuman: function (date: string): string {
+    return moment(date).format("dd/MM/YYYY");
+  },
+
+  getHourFromStringDateTime: function (datetime: string): string {
+    return moment(datetime).format("HH:mm");
+  },
+  getHourFromDateTime: function (datetime: Moment): string {
+    return datetime.format("HH:mm");
+  },
+  addSecondsToStringDateTime: function (
+    datetime: string,
+    seconds: number
+  ): Moment {
+    return moment(datetime).add(seconds, "seconds");
+  },
+};
+
+export default dateFormatter;
