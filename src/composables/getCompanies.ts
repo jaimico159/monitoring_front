a6ff28e673ca87005e3091d11acfedcfd36f23b7
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-12 09:51:04 -0500

    change location of interfaces

--- src/composables/getCompanies.ts
+++ src/composables/getCompanies.ts
@@ -1,10 +1,6 @@
 import API_SCHEMA_V1 from "@/utils/api_schema_v1";
 import axios from "axios";
 
-interface APICompany {
-  name: string;
-}
-
 const getCompanies = async (): Promise<Company[]> => {
   const { data } = await axios.get(API_SCHEMA_V1.companies.getCompanies);
 
--- src/composables/getEngineers.ts
+++ src/composables/getEngineers.ts
@@ -1,12 +1,6 @@
 import API_SCHEMA_V1 from "@/utils/api_schema_v1";
 import axios from "axios";
 
-interface APIEngineer {
-  first_name: string;
-  last_name: string;
-  display_name: string;
-}
-
 const getEngineers = async (): Promise<Engineer[]> => {
   const { data } = await axios.get(API_SCHEMA_V1.engineers.getEngineers);
 
