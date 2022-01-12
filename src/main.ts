f2ff6ee5aa940364dcef88a1aa3478c7afe570fb
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-11 22:31:54 -0500

    adds config to axios

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,12 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
+import axios from "axios";
+
+axios.interceptors.request.use(async (config) => {
+  config.baseURL = process.env.VUE_APP_BASE_URL_V1;
+  return config;
+});
 
 createApp(App).use(router).mount("#app");
 
