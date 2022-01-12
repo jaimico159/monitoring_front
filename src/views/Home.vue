12fbc75768aa9bd828f122497fc1a86648cac84a
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-12 09:37:48 -0500

    display compnies and engineers

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -3,26 +3,39 @@
     <div v-for="(company, index) in companies" :key="index">
       {{ company.name }}
     </div>
+
+    <div v-for="(engineer, index) in engineers" :key="index">
+      {{ engineer.displayName }}
+    </div>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent, onMounted, ref } from "vue";
 import getCompanies from "@/composables/getCompanies";
-import { Company } from "@/typings/api";
+import { Company, Engineer } from "@/typings/api";
+import getEngineers from "@/composables/getEngineers";
 
 export default defineComponent({
   name: "Home",
   setup() {
     let companies = ref<Company[]>([]);
+    let engineers = ref<Engineer[]>([]);
 
     const _getCompanies = async (): Promise<void> => {
       companies.value = await getCompanies();
     };
 
-    onMounted(_getCompanies);
+    const _getEngineers = async (): Promise<void> => {
+      engineers.value = await getEngineers();
+    };
 
-    return { companies };
+    onMounted(() => {
+      _getCompanies();
+      _getEngineers();
+    });
+
+    return { companies, engineers };
   },
 });
 </script>
