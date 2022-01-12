2e9a188a610ae6c65543a81df1430fe56978bec5
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-12 09:19:32 -0500

    updates composable

--- src/composables/getCompanies.ts
+++ src/composables/getCompanies.ts
@@ -1,11 +1,14 @@
 import API_SCHEMA_V1 from "@/utils/api_schema_v1";
 import axios from "axios";
 
-const getCompanies = async (): Promise<Company[] | undefined> => {
+const getCompanies = async (): Promise<Company[]> => {
   const { data } = await axios.get(API_SCHEMA_V1.companies.getCompanies);
-  return data.companies.map((company: Company) => {
+
+  return data.map((company: Company) => {
     return {
       name: company.name,
     };
   });
 };
+
+export default getCompanies;
