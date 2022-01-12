2c24cca305cef119c824b6ba52909e3d32da0ba3
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-12 09:36:44 -0500

    adds interface

--- src/composables/getCompanies.ts
+++ src/composables/getCompanies.ts
@@ -1,10 +1,14 @@
 import API_SCHEMA_V1 from "@/utils/api_schema_v1";
 import axios from "axios";
 
+interface APICompany {
+  name: string;
+}
+
 const getCompanies = async (): Promise<Company[]> => {
   const { data } = await axios.get(API_SCHEMA_V1.companies.getCompanies);
 
-  return data.map((company: Company) => {
+  return data.map((company: APICompany) => {
     return {
       name: company.name,
     };
