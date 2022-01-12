784dad940883df613b626fa40c48227156a7368b
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-12 09:20:01 -0500

    types for vue

---
+++ src/typings/api.d.ts
@@ -0,0 +1,40 @@
+interface Company {
+  name: string;
+}
+
+interface Engineer {
+  firstName: string;
+  lastName: string;
+  displayName: string;
+}
+
+interface Contract {
+  startAt: string;
+  endAt: string;
+  slotDuration: number;
+  company: Company;
+}
+
+interface ContractPlan {
+  contract: Contract;
+  startDate: string;
+  endDate: string;
+  slotDuration: number;
+  timeSlotsCount: number;
+}
+
+interface TimeSlot {
+  startAt: string;
+  duration: number;
+  contract: Contract;
+  contractPlan: ContractPlan;
+  engineer: Engineer;
+  reservations: Reservation[];
+}
+
+interface Reservation {
+  engineer: Engineer;
+  timeSlot: TimeSlot;
+}
+
+export { Company, Engineer, Contract, ContractPlan, TimeSlot, Reservation };
