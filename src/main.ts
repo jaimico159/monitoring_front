6b2f683e087b4103b127fb2aa7db403deb520cb3
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-13 03:41:00 -0500

    config params

--- src/main.ts
+++ src/main.ts
@@ -3,9 +3,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 import axios from "axios";
+import { decamelizeKeys } from "humps";
 
 axios.interceptors.request.use(async (config) => {
   config.baseURL = process.env.VUE_APP_BASE_URL_V1;
+  config.params = decamelizeKeys(config.params);
+  config.data = decamelizeKeys(config.data);
   return config;
 });
 
