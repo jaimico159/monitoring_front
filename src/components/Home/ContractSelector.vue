997c635b5f19016e84ae857b5b70ce696a523f1f
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-12 21:48:12 -0500

    moves selector to componen

---
+++ src/components/Home/ContractSelector.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="selector">
+    <select
+      class="form-select"
+      aria-label="companies"
+      v-model="selectedContractId"
+    >
+      <option selected>Selecciona un contrato</option>
+      <option
+        v-for="(contract, index) in contracts"
+        :key="index"
+        :value="contract.id"
+      >
+        {{ contract.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from "vue";
+
+export default defineComponent({
+  name: "ContractSelector",
+  setup() {
+    const selectedContractId = inject("selectedContractId", undefined);
+    const contracts = inject("contracts", []);
+
+    return { selectedContractId, contracts };
+  },
+});
+</script>
