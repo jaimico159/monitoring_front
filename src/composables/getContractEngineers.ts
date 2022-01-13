68adaac3c166383eff04736264a3365ce2dc8762
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-12 22:39:29 -0500

    creates composable to get engineers from contract

---
+++ src/composables/getContractEngineers.ts
@@ -0,0 +1,23 @@
+import API_SCHEMA_V1 from "@/utils/api_schema_v1";
+import axios from "axios";
+
+const getContractEngineers = async (
+  contractId: number
+): Promise<Engineer[]> => {
+  const { data } = await axios.get(
+    API_SCHEMA_V1.contracts.getContractEngineers(contractId)
+  );
+
+  console.log(data);
+
+  return data.engineers.map((engineer: APIEngineer): Engineer => {
+    return {
+      id: engineer.id,
+      firstName: engineer.first_name,
+      lastName: engineer.last_name,
+      displayName: engineer.display_name,
+    };
+  });
+};
+
+export default getContractEngineers;
