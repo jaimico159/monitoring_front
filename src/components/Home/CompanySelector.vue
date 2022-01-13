2a2329c129fffaa18b4eff1d13d19b03b3ae0bda
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-12 21:47:56 -0500

    moves selector to component

---
+++ src/components/Home/CompanySelector.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="selector">
+    <select
+      class="form-select"
+      aria-label="companies"
+      v-model="selectedCompanyId"
+    >
+      <option selected>Selecciona una compañía</option>
+      <option
+        v-for="(company, index) in companies"
+        :key="index"
+        :value="company.id"
+      >
+        {{ company.name }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from "vue";
+
+export default defineComponent({
+  name: "CompanySelector",
+  setup() {
+    const selectedCompanyId = inject("selectedCompanyId", undefined);
+    const companies = inject("companies", []);
+
+    return { selectedCompanyId, companies };
+  },
+});
+</script>
