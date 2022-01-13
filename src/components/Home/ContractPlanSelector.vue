b0284cded1a4db9ee70c2ffcce4b928e23efe8f1
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-12 21:48:04 -0500

    moves selector to component

---
+++ src/components/Home/ContractPlanSelector.vue
@@ -0,0 +1,32 @@
+<template>
+  <div id="selector">
+    <select
+      class="form-select"
+      aria-label="companies"
+      v-model="selectedContractPlanId"
+    >
+      <option selected>Selecciona un contrato</option>
+      <option
+        v-for="(contractPlan, index) in contractPlans"
+        :key="index"
+        :value="contractPlan.id"
+      >
+        {{ `Del ${contractPlan.startDate} al ${contractPlan.endDate}` }}
+      </option>
+    </select>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent, inject } from "vue";
+
+export default defineComponent({
+  name: "ContractPlanSelector",
+  setup() {
+    const selectedContractPlanId = inject("selectedContractPlanId", undefined);
+    const contractPlans = inject("contractPlans", []);
+
+    return { selectedContractPlanId, contractPlans };
+  },
+});
+</script>
