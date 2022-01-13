b32d89c14ed58f27194fbaba53612b74bfa46614
jaimico159 <jaime.huarsaya.rivera@gmail.com>
2022-01-13 03:54:31 -0500

    updates types

--- src/typings/api.d.ts
+++ src/typings/api.d.ts
@@ -1,40 +1,133 @@
+type Moment = import("moment").Moment;
+
+interface APICompany {
+  id: number;
+  name: string;
+}
+
+interface APIContract {
+  id: number;
+  name: string;
+  start_at: string;
+  end_at: string;
+  slot_duration: number;
+  company_id: number;
+  company?: Company;
+}
+
+interface APIContractPlan {
+  id: number;
+  contract_id: number;
+  start_date: string;
+  end_date: string;
+  slot_duration: number;
+  contract_plan_days: APIContractPlanDay[];
+}
+
+interface APIContractPlanDay {
+  id: number;
+  current_date: string;
+  contract_plan_id: number;
+  time_slots: APITimeSlot[];
+}
+
+interface APIReservation {
+  id: number;
+  engineer_id: number;
+  time_slot_id: number;
+}
+
+interface APITimeSlot {
+  id: number;
+  start_at: string;
+  duration: integer;
+  contract_id: number;
+  contract_plan_id: number;
+  contract_plan_day_id: number;
+  engineer_id: number | null;
+  engineer: APIEngineer;
+  reservations?: APIRevervation;
+}
+interface APIEngineer {
+  id: number;
+  first_name: string;
+  last_name: string;
+  display_name: string;
+}
+
+interface SetReservationsForm {
+  reservations: { timeSlotId: number; engineerId: number }[];
+}
+
 interface Company {
+  id: number;
   name: string;
 }
 
 interface Engineer {
+  id: number;
   firstName: string;
   lastName: string;
   displayName: string;
 }
 
 interface Contract {
+  id: number;
+  name: string;
   startAt: string;
   endAt: string;
   slotDuration: number;
-  company: Company;
+  companyId: number;
+  company?: Company;
 }
 
 interface ContractPlan {
-  contract: Contract;
+  id: number;
+  contract?: Contract;
   startDate: string;
   endDate: string;
   slotDuration: number;
-  timeSlotsCount: number;
+  contractId: number;
+  contractPlanDays: ContractPlanDay[];
+}
+
+interface ContractPlanDay {
+  id: number;
+  currentDate: string;
+  contractPlanId: number;
+  timeSlots: TimeSlot[];
 }
 
 interface TimeSlot {
+  id: number;
   startAt: string;
+  endAt: string;
   duration: number;
-  contract: Contract;
-  contractPlan: ContractPlan;
-  engineer: Engineer;
-  reservations: Reservation[];
+  contract?: Contract;
+  contractPlan?: ContractPlan;
+  engineerId: number | null;
+  engineer?: Engineer;
+  reservations?: Reservation[];
+  contractId: number;
+  contractPlanId: number;
+  contractPlanDayId: number;
 }
 
 interface Reservation {
-  engineer: Engineer;
-  timeSlot: TimeSlot;
+  id: number;
+  engineer?: Engineer;
+  timeSlot?: TimeSlot;
+  engineerId: number;
+  timeSlotId: number;
 }
 
-export { Company, Engineer, Contract, ContractPlan, TimeSlot, Reservation };
+export {
+  Company,
+  Engineer,
+  Contract,
+  ContractPlan,
+  ContractPlanDay,
+  TimeSlot,
+  Reservation,
+  SetReservationsForm,
+};
